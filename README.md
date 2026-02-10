defd3690768067cb1da0d8c473f5c5e531d20962
Dorganhozo <131984031+Dorganhozo@users.noreply.github.com>
2026-02-10 13:52:50 -0300

    Testando um negocio

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 Testando
+```
+mkdir build
+cd build
+cmake ..
+make
+
+./test a b c d e
+```
--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,6 @@
 using namespace std;
 
 int main(int argc, char** args){
-
 	time_t t;
 	time(&t);
 	srand(t);
